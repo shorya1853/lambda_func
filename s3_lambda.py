24d41f1aee97c0c3a410a7f91de9d163830b6c29
shorya1853 <shoryadubey136@gmail.com>
2023-10-30 11:49:54 +0530

    new file:   .env 	new file:   s3_lambda.py

---
+++ s3_lambda.py
@@ -0,0 +1,37 @@
+import json
+import boto3
+import os
+
+profile = os.getenv("PROFILE_NAME")
+
+session = boto3.Session(profile_name=profile)
+
+sqs = session.client("sqs")
+sqs_queue_url = "https://sqs.ap-south-1.amazonaws.com/893711537471/url_queue"
+
+def lambda_handler(event, context):
+    # TODO implement
+    key = event['Records'][0]['s3']['object']['key']
+    message_body = "hello world"
+    response = sqs.receive_message(
+        QueueUrl=sqs_queue_url,
+        AttributeNames=[
+            'SentTimestamp'
+        ],
+        MaxNumberOfMessages=1,
+        MessageAttributeNames=[
+            'All'
+        ],
+        VisibilityTimeout=0,
+        WaitTimeSeconds=0
+    )
+    
+    response = sqs.send_message(
+    QueueUrl=sqs_queue_url,
+    MessageBody=message_body
+    )
+    print(response)
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
