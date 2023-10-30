1c1eb1df790f01c948bd0b9d78db95b3e23ea5e7
shorya1853 <shoryadubey136@gmail.com>
2023-10-30 19:41:04 +0530

    modified:   s3_lambda.py

--- s3_lambda.py
+++ s3_lambda.py
@@ -6,25 +6,25 @@ profile = os.getenv("PROFILE_NAME")
 
 session = boto3.Session(profile_name=profile)
 
-sqs = session.client("sqs")
+sqs = session.client("sqs", region_name='ap-south-1')
 sqs_queue_url = "https://sqs.ap-south-1.amazonaws.com/893711537471/url_queue"
 
 def lambda_handler(event, context):
     # TODO implement
     key = event['Records'][0]['s3']['object']['key']
-    message_body = "hello world"
-    response = sqs.receive_message(
-        QueueUrl=sqs_queue_url,
-        AttributeNames=[
-            'SentTimestamp'
-        ],
-        MaxNumberOfMessages=1,
-        MessageAttributeNames=[
-            'All'
-        ],
-        VisibilityTimeout=0,
-        WaitTimeSeconds=0
-    )
+    message_body = key
+    # response = sqs.receive_message(
+    #     QueueUrl=sqs_queue_url,
+    #     AttributeNames=[
+    #         'SentTimestamp'
+    #     ],
+    #     MaxNumberOfMessages=1,
+    #     MessageAttributeNames=[
+    #         'All'
+    #     ],
+    #     VisibilityTimeout=0,
+    #     WaitTimeSeconds=0
+    # )
     
     response = sqs.send_message(
     QueueUrl=sqs_queue_url,
